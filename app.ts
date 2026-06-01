73db8ae511d640a9faef3111817c0f91c6df554b
Nik <laveosa@yahoo.com>
2026-06-01 17:22:49 +0300

    start stage configurations

--- app.ts
+++ app.ts
@@ -1,3 +1,17 @@
 import express from "express";
+import { createServer } from "node:http";
+import { Server } from "socket.io";
 
 const app = express();
+const server = createServer(app);
+const io = new Server(server);
+
+const PORT = 5000;
+
+app.get("api", (req, res) => {
+  res.json({
+    message: "hello",
+  });
+});
+
+app.listen(PORT, () => console.log(`Server runing on prot: ${PORT}`));
